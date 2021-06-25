1c6926b0dce01d178387275e415a546c594094da
rahul negi <36270407+rahulnegi20@users.noreply.github.com>
2021-06-26 02:07:41 +0530

    Create Find the maximum and minimum element in an array.py

---
+++ Arrays/Find the maximum and minimum element in an array.py
@@ -0,0 +1,10 @@
+def min_and_max(arr):
+  """
+  Sort the array and print out the first and last element
+  as they will be the min and max element of that array.
+  """
+  arr.sort()
+  print("Minimum =", arr[0])
+  print("Maximum =", arr[-1])
+  
+ return 

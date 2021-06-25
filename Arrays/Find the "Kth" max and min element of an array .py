8d4afcf99d715cdef70d636d6fb067c52dcdcf78
rahul negi <36270407+rahulnegi20@users.noreply.github.com>
2021-06-26 02:01:54 +0530

    Create Find the "Kth" max and min element of an array .py

---
+++ Arrays/Find the "Kth" max and min element of an array .py
@@ -0,0 +1,12 @@
+# Execution time : 0.02 s 
+def kthSmallest(self,arr, l, r, k):
+    '''
+    arr : given array
+    l : starting index of the array i.e 0
+    r : ending index of the array i.e size-1
+    k : find kth smallest element and return using this function
+    '''
+    arr.sort()
+    return(arr[k-1])
+
+#link : https://practice.geeksforgeeks.org/problems/kth-smallest-element5635/1

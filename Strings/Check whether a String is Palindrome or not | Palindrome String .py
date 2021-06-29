722d9e3a1a4cb083007c98a2da08a95078135364
rahul negi <36270407+rahulnegi20@users.noreply.github.com>
2021-06-30 04:58:53 +0530

    Create Check whether a String is Palindrome or not | Palindrome String .py

---
+++ Strings/Check whether a String is Palindrome or not | Palindrome String .py
@@ -0,0 +1,6 @@
+#execution time : 0.06s
+#pythonic way :P 
+def isPlaindrome(self, S):
+  return('1' if S==S[::-1] else '0')
+
+# link : https://practice.geeksforgeeks.org/problems/palindrome-string0817/1#
